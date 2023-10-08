8ae43a38b9acff80d51c1f1ab099129bb8eddfdb
ilmantim <ilmantim@gmail.com>
2023-10-08 15:49:07 +0200

    Add new argument to the function
    The fetch_nasa_epic function has been modified to accept an
    additional 'token' argument.
    
    Additionally, a 'main' function has been defined to load environment variables
    and call fetch_nasa_epic with the required arguments, improving code organization
    and making it easier to configure the function.

--- fetch_nasa_epic.py
+++ fetch_nasa_epic.py
@@ -5,7 +5,7 @@ from common_functions import get_file_extension, download_image
 from dotenv import load_dotenv
 
 
-def fetch_nasa_epic(count):
+def fetch_nasa_epic(count, token):
     base_directory =  os.path.dirname(os.path.abspath(__file__))
     images_directory = os.path.join(base_directory, "images")
     os.makedirs(images_directory, exist_ok=True)
@@ -46,7 +46,7 @@ def fetch_nasa_epic(count):
         except:
              print(f"Image {index} download failed.")
 
-if __name__ == "__main__":
+def main():
     load_dotenv()
     token = os.getenv('NASA_TOKEN')
 
@@ -55,7 +55,11 @@ if __name__ == "__main__":
         parser.add_argument('--count', help='Specify the number of images to download')
         args = parser.parse_args()
         
-        fetch_nasa_epic(args.count) 
+        fetch_nasa_epic(args.count, token) 
 
     except requests.exceptions.RequestException as e:
         print("Error:", e)
+
+
+if __name__ == "__main__":
+    main()
