70b06a3f198b88da24e7e6ffa44393f74312ca6f
ilmantim <ilmantim@gmail.com>
2023-10-08 19:17:58 +0200

    Separate the download_images function into two functions
    - Separated the download_images function into two functions: download_images (for downloading) and save_images (for saving).
    - Improved code modularity and readability.

--- fetch_spacex_launch.py
+++ fetch_spacex_launch.py
@@ -3,7 +3,6 @@ import requests
 import argparse
 from common_functions import get_file_extension, download_image
 
-
 def fetch_spacex_launch(launch_id):
     base_directory = os.path.dirname(os.path.abspath(__file__))
     images_directory = os.path.join(base_directory, "images")
@@ -12,39 +11,40 @@ def fetch_spacex_launch(launch_id):
     launch_url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
     response = requests.get(launch_url)
     response.raise_for_status()
-    launch_pictures= response.json()
+    launch_pictures = response.json()
 
     flickr_links = launch_pictures["links"]["flickr"]
     original_urls = flickr_links.get("original")
 
-    download_images(original_urls, images_directory)
+    downloaded_images = download_images(original_urls)
+    save_images(downloaded_images, images_directory)
 
-
-def download_images(original_urls, images_directory, params=None):
+def download_images(original_urls, params=None):
+    downloaded_images = []
     for index, image_url in enumerate(original_urls, start=1):
         extension = get_file_extension(image_url)
         filename = f"image{index}{extension}"
-        save_path = os.path.join(images_directory, filename)
 
-        result = download_image(image_url, params, save_path)
+        try:
+            image_data = download_image(image_url, params)
+            downloaded_images.append((image_data, filename))
+            print(f"Image {index} downloaded successfully.")
+        except:
+            print(f"Image {index} download failed.")
 
-        if result:
-            print("Image was downloaded and saved successfully.")
-        else:
-            print("Image download failed.")
+    return downloaded_images
 
+def save_images(images, save_directory):
+    for image_data, filename in images:
+        save_path = os.path.join(save_directory, filename)
+        with open(save_path, "wb") as file:
+            file.write(image_data)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Download SpaceX launch images')
     parser.add_argument('--launch_id', default='latest', help='Specify the launch ID to download images from')
     args = parser.parse_args()
-
     fetch_spacex_launch(args.launch_id)
-
-
-   
-
-
 
 
 
