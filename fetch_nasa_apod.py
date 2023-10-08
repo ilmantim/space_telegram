b9c35ae65cbb0143517ab7223c860225a4170b36
ilmantim <ilmantim@gmail.com>
2023-10-08 15:43:28 +0200

    Add new arguments to the function
    The fetch_nasa_apod function has been modified to accept an
    additional 'token' argument.
    
    Additionally, a 'main' function has been defined to load environment variables
    and call fetch_nasa_apod with the required arguments, improving code organization
    and making it easier to configure the function.

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -5,7 +5,7 @@ from common_functions import get_file_extension, download_image
 from dotenv import load_dotenv
 
 
-def fetch_nasa_apod(count):
+def fetch_nasa_apod(count, token):
     base_directory =  os.path.dirname(os.path.abspath(__file__))
     images_directory = os.path.join(base_directory, "images")
     os.makedirs(images_directory, exist_ok=True)
@@ -37,7 +37,7 @@ def fetch_nasa_apod(count):
              print(f"Image {index} download failed.")
 
 
-if __name__ == "__main__":
+def main():
     load_dotenv()
     token = os.getenv('NASA_TOKEN')
 
@@ -46,11 +46,14 @@ if __name__ == "__main__":
         parser.add_argument('--count', help='Specify the number of images to download')
         args = parser.parse_args()
         
-        fetch_nasa_apod(args.count)
+        fetch_nasa_apod(args.count, token)
         
     except requests.exceptions.RequestException as e:
         print("Error:", e)
-    
+
+
+if __name__ == "__main__":
+    main()
 
 
 
